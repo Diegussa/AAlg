1d2a5c0949b78afaec9724a0abb3875ba43865b8
Hernanzueloo <108001322+Hernanzueloo@users.noreply.github.com>
2022-10-07 17:46:22 +0200

    Update times.c

--- times.c
+++ times.c
@@ -33,10 +33,10 @@ short average_sorting_time(pfunc_sort metodo, int n_perms, int N, PTIME_AA ptime
   if (!p)
     return ERR;
 
-  t1 = clock(); //Almacenamiento del tiempo al comenzar
+  t1 = clock(); /*Almacenamiento del tiempo al comenzar*/
   /*LLamamos a la función una primera vez para inicializar los valores*/
   n_ob = metodo(p[0], 0, N - 1);
-  if ((n_ob < 0)) //Control de errores
+  if ((n_ob < 0)) /*Control de errores*/
   {
     for (i = N - 1; i >= 0; i--)
     {
@@ -54,7 +54,7 @@ short average_sorting_time(pfunc_sort metodo, int n_perms, int N, PTIME_AA ptime
   {
 
     n_ob = metodo(p[i], 0, N - 1);
-    if ((n_ob) < 0) //Control de errores
+    if ((n_ob) < 0) /*Control de errores*/
     {
       for (i = n_perms - 1; i >= 0; i--)
       {
@@ -64,17 +64,17 @@ short average_sorting_time(pfunc_sort metodo, int n_perms, int N, PTIME_AA ptime
       return ERR;
     }
    
-    if (n_ob < min_ob) //Actualizacion del min_ob si es pertinente
+    if (n_ob < min_ob) /*Actualizacion del min_ob si es pertinente*/
     {
       min_ob = n_ob;
     }
-    else if (n_ob > max_ob) //Actualizacion del max_ob si es pertinente
+    else if (n_ob > max_ob) /*Actualizacion del max_ob si es pertinente*/
     {
       max_ob = n_ob;
     }
     suma_ob += n_ob;
   }
-  t2 = clock(); //Almacenamiento del tiempo al finalizar
+  t2 = clock(); /*Almacenamiento del tiempo al finalizar*/
   
   /*Almacenamiento de las caracteristicsd de la funcion de ordenacion indicada con los parametros dados*/
   ptime->time = (double)(t2 - t1) / (double)n_perms;
@@ -84,7 +84,7 @@ short average_sorting_time(pfunc_sort metodo, int n_perms, int N, PTIME_AA ptime
   ptime->min_ob = min_ob;
   ptime->max_ob = max_ob;
 
-  for (i = 0; i <n_perms; i++) //Liberacion de la memoria
+  for (i = 0; i <n_perms; i++) /*Liberacion de la memoria*/
       {
         free(p[i]);
       }
@@ -102,17 +102,17 @@ short generate_sorting_times(pfunc_sort method, char *file, int num_min, int num
   TIME_AA *ptime = NULL;
   int i, j, flag, correction, tam;
 
-  if (!file || num_min < 0 || num_max < 0) //Control de errores
+  if (!file || num_min < 0 || num_max < 0) /*Control de errores*/
     return ERR;
 
-  correction = (num_max % incr == num_min % incr ? 1 : 0); //Definicion de la correcion
+  correction = (num_max % incr == num_min % incr ? 1 : 0); /*Definicion de la correcion*/
 
   if (incr == 1)
     correction = 0;
 
   tam = (num_max - num_min + 1) / incr + correction; /*Reserva dinamica de la tabla de datos*/
 
-  ptime = (TIME_AA *)calloc(tam, sizeof(TIME_AA)); //Reserva de memoria
+  ptime = (TIME_AA *)calloc(tam, sizeof(TIME_AA)); /*Reserva de memoria*/
   if (!ptime)
     return ERR;
 
@@ -126,14 +126,14 @@ short generate_sorting_times(pfunc_sort method, char *file, int num_min, int num
     }
   }
 
-  if (save_time_table(file, ptime, tam) < 0) //Guardar en fichero controlando errores
+  if (save_time_table(file, ptime, tam) < 0) /*Guardar en fichero controlando errores*/
   {
 
     free(ptime);
     return ERR;
   }
 
-  free(ptime); //Liberacion de memoria
+  free(ptime); /*Liberacion de memoria*/
   return OK;
 }
 
@@ -146,14 +146,14 @@ short save_time_table(char *file, PTIME_AA ptime, int n_times)
 {
   FILE *f = NULL;
   int i;
-  if (!file || !ptime || n_times < 1) //Control de errores
+  if (!file || !ptime || n_times < 1) /*Control de errores*/
     return ERR;
 
   f = fopen(file, "w"); /*Apertura de archivo*/
   if (!f)
     return ERR;
 
-  for (i = 0; i < n_times; i++) //Escritura de la tabla en el fichero proporcionado
+  for (i = 0; i < n_times; i++) /*Escritura de la tabla en el fichero proporcionado*/
   {
     
     if (5 > fprintf(f, "%d    %f    %f    %d   %d\n", ptime[i].N, ptime[i].time, ptime[i].average_ob, ptime[i].max_ob, ptime[i].min_ob))
