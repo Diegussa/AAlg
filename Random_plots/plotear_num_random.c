65b5aba9ffc756e732916501a6ea309b738f5caa
Diegussa <diegorortiz03@gmail.com>
2022-10-12 13:34:51 +0200

    Nuestra_random

--- Random_plots/plotear_num_random.c
+++ Random_plots/plotear_num_random.c
@@ -3,7 +3,7 @@
 #include <time.h>
 #include <stdlib.h>
 #define MAX 100000000
-#define SUP 100
+#define SUP 10000
 #define INF 1
 
 int random_num(int inf, int sup)
@@ -16,6 +16,19 @@ int random_num(int inf, int sup)
     return inf + (int)((sup - inf + 1.0) * rand() / (RAND_MAX + 1.0));
     /*return inf + rand()%(sup-inf+1);*/
 }
+int aleat_num(int inf, int sup) {
+    int a=0;
+    if ( inf > sup ) {
+        return -1;
+    }
+    while(!a){
+        a=  (int)rand();
+        
+        if (a>(RAND_MAX-(RAND_MAX%(sup - inf +1)))) a=0;
+    }
+    return inf + a%(sup - inf +1);
+}
+
 int main(){
     int t[SUP-INF+1],i;
     FILE*f;
